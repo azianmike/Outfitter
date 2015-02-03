d3f3e5ed3480d1a3fe91b4c345f2d3e7caefa34a
michaelluo <michaeluo@gmail.com>
2015-02-02 20:42:43 -0600

    Initial commit with iPhone project

---
+++ Outfitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Outfitter
+//
+//  Created by Michael Luo on 2/2/15.
+//  Copyright (c) 2015 Outfitter Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
