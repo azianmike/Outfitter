65facfce9a63c9d61716f3b9d5333b5fc3567001
Yusuf Sobh <yusufoos@gmail.com>
2015-02-17 14:32:56 -0600

    Added cocoapods and parse

---
+++ Outfitter/Outfitter-Bridging-Header.h.h
@@ -0,0 +1,14 @@
+//
+//  Outfitter-Bridging-Header.h.h
+//  Outfitter
+//
+//  Created by Yusuf Sobh on 2/17/15.
+//  Copyright (c) 2015 Outfitter Group. All rights reserved.
+//
+
+#ifndef Outfitter_Outfitter_Bridging_Header_h_h
+#define Outfitter_Outfitter_Bridging_Header_h_h
+
+#import <Parse/Parse.h>
+
+#endif
