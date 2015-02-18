a34452d3ee60569d7479873558a18c9282996ae2
michaelluo <michaeluo@gmail.com>
2015-02-18 17:16:03 -0600

    Added login delegate callback function and updated name of viewcontroller

---
+++ Outfitter/HomeViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Outfitter
+//
+//  Created by Michael Luo on 2/2/15.
+//  Copyright (c) 2015 Outfitter Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, PFLogInViewControllerDelegate {
+    
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(false)
+        // Do any additional setup after loading the view, typically from a nib.
+        NSLog("enter view didAppear")
+        showLoginView()
+        
+    }
+    
+    func showLoginView(){
+        if (PFUser.currentUser() == nil) {
+            var logInController = PFLogInViewController()
+            logInController.fields = (PFLogInFields.UsernameAndPassword | PFLogInFields.LogInButton | PFLogInFields.SignUpButton | PFLogInFields.Facebook)
+            logInController.delegate=self
+            let newLogo = UILabel()
+            newLogo.textColor = UIColor.blackColor()
+            newLogo.text = "Outfitter"
+            newLogo.sizeToFit()
+            logInController.logInView.logo=newLogo
+            self.presentViewController(logInController, animated:true, completion: nil)
+        }
+    }
+    
+    @IBAction func logout(){
+        PFUser.logOut()
+        var currentUser = PFUser.currentUser() // this will now be nil
+        showLoginView()
+    }
+    
+    func logInViewController(controller: PFLogInViewController, didLogInUser user: PFUser!) -> Void {
+        //self.dismissViewController(true, completion: nil)
+        NSLog("enter")
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func logInViewControllerDidCancelLogIn(controller: PFLogInViewController) -> Void {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
