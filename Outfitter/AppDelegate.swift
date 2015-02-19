2702848a72ef7f630ec08f96b1127331d015f5f3
michaelluo <michaeluo@gmail.com>
2015-02-18 19:05:58 -0600

    Updated facebook log in and is now able to grab user name

--- Outfitter/AppDelegate.swift
+++ Outfitter/AppDelegate.swift
@@ -18,17 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         Parse.setApplicationId("SOzwRu6FgQqXZutDyUx25OhuydwfBhY5amOqp2Td", clientKey: "wpuJaLeNKjskcN5dCWbMfqIxkfuz91tvX8sFDNXS")
         PFFacebookUtils.initializeFacebook()
-        let permissions = ["email", "name"]
-        PFFacebookUtils.logInWithPermissions(permissions, {
-            (user: PFUser!, error: NSError!) -> Void in
-            if user == nil {
-                NSLog("Uh oh. The user cancelled the Facebook login.")
-            } else if user.isNew {
-                NSLog("User signed up and logged in through Facebook!")
-            } else {
-                NSLog("User logged in through Facebook!")
-            }
-        })
+
         
         return true
     }
--- Outfitter/HomeViewController.swift
+++ Outfitter/HomeViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController, PFLogInViewControllerDelegate {
     
-    
+    @IBOutlet var welcomeMessage: UILabel!
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(false)
         // Do any additional setup after loading the view, typically from a nib.
+        updateWelcomeMessage()
         showLoginView()
         
     }
@@ -36,11 +37,31 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate {
     @IBAction func logout(){
         PFUser.logOut()
         var currentUser = PFUser.currentUser() // this will now be nil
+        
         showLoginView()
     }
     
     func logInViewController(controller: PFLogInViewController, didLogInUser user: PFUser!) -> Void {
         self.dismissViewControllerAnimated(true, completion: nil)
+        updateWelcomeMessage()
+
+    }
+    
+    func updateWelcomeMessage(){
+        if (FBSession.activeSession().isOpen)
+        {
+            FBRequest.requestForMe().startWithCompletionHandler({ (FBRequestConnection connected, FBGraphUser user, NSError error) -> Void in
+                let currentUser=PFUser.currentUser()
+                currentUser.username=user.name
+                currentUser.email = user.email
+                
+                PFUser.currentUser().save()
+                self.welcomeMessage.text = "Welcome back " + user.name
+            })
+        }else{
+            self.welcomeMessage.text = "Welcome back " + PFUser.currentUser().username
+
+        }
     }
     
     func logInViewControllerDidCancelLogIn(controller: PFLogInViewController) -> Void {
