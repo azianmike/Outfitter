adba985d70433ce12ba4bcfbf15fb8272c595827
Ian Eckles <iae610@gmail.com>
2015-03-07 10:06:44 -0600

    Fixed the portfolio bug where it would crash if the user does not have a name in the database. Now it will use their username if they do not have a name.

--- Outfitter/PortfolioViewController.swift
+++ Outfitter/PortfolioViewController.swift
@@ -23,36 +23,6 @@ public class PortfolioViewController: UIViewController {
 
         submissions = [PFObject]()
         getSubmissions(doneGettingSubmissions)
-        /*var query = PFQuery(className:"Submission")
-        query.whereKey("submittedByUser", equalTo:PFUser.currentUser().username)
-        
-        let loadingNotification = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        loadingNotification.mode = MBProgressHUDModeIndeterminate
-        loadingNotification.labelText = "Loading Submissions"
-        
-        query.findObjectsInBackgroundWithBlock {
-            (objects: [AnyObject]!, error: NSError!) -> Void in
-            if error == nil {
-                if let objects = objects as? [PFObject] {
-                    self.submissions = [PFObject]()
-                    if objects.count == 0 {
-                        let alert = UIAlertController(title: "Error", message: "We cannot load your submissions because you do not have any!", preferredStyle: UIAlertControllerStyle.Alert)
-                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-                        self.presentViewController(alert, animated: true, completion: nil)
-                    } else {
-                        for object in objects {
-                            self.submissions.append(object)
-                        }
-                    }
-                    self.collectionView.reloadData()
-                }
-                MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
-            } else {
-                // Log details of the failure
-                println("Error: \(error) \(error.userInfo!)")
-                MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
-            }
-        }*/
     }
     
     func doneGettingSubmissions(){
@@ -94,7 +64,11 @@ public class PortfolioViewController: UIViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        navigationBar.topItem?.title = PFUser.currentUser().objectForKey("name") as String! + "'s Portfolio"
+        if let temp = PFUser.currentUser().objectForKey("name") as String! {
+            navigationBar.topItem?.title = temp + "'s Portfolio"
+        } else {
+            navigationBar.topItem?.title = PFUser.currentUser().username + "'s Portfolio"
+        }
     }
     
     override public func didReceiveMemoryWarning() {
