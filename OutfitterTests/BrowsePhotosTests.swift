c9e0b5be3394ce08b4d96e75fad603ae46f5455e
michaelluo <michaeluo@gmail.com>
2015-03-17 17:44:24 -0500

    Added new test to test that submitting a rating decreases the getSubmissionsList by one

--- OutfitterTests/BrowsePhotosTests.swift
+++ OutfitterTests/BrowsePhotosTests.swift
@@ -153,6 +153,68 @@ class BrowseViewControllerTest: BaseTests {
         
     }
     
+    func testSubmitRatingSubmissionsListDecreasesByOne()
+    {
+        // we only have access to this if we import our project above
+        let v = viewController
+        
+        
+        let expectation = self.expectationWithDescription("Load submissions")
+        
+        // assert that the ViewController.view is not nil
+        XCTAssertNil(v.submissions,"No submissions loaded yet")
+        v.submissions = [PFObject]()
+        v.currentSubmissionIndex = -1
+        func submissionsCallBack() {
+            XCTAssertTrue(v.submissions.count > 0,"Submissions were loaded")
+            expectation.fulfill()
+        }
+        
+        v.getSubmissions(submissionsCallBack)
+        
+        self.waitForExpectationsWithTimeout(5.0) { (error) in
+            if(error != nil) {
+                XCTFail("FAILED due to " + error.description)
+            }
+        }
+        
+        let oldSubmissionsCount = v.submissions.count
+        let expectation2 = self.expectationWithDescription("Submit Rating")
+        
+        func ratingCallback(result:Bool){
+            XCTAssertTrue(result);
+            
+            expectation2.fulfill()
+        }
+        
+        v.submitRatingActivity(v.submissions[0].objectId, userId: PFUser.currentUser().objectId, votedYes: false, ratingCallback)
+        
+        self.waitForExpectationsWithTimeout(5.0) { (error) in
+            if(error != nil) {
+                XCTFail("FAILED due to " + error.description)
+            }
+        }
+        
+        let expectation3 = self.expectationWithDescription("Load submissions")
+        
+        // assert that the ViewController.view is not nil
+        v.submissions = [PFObject]()
+        func submissionsCallBack3() {
+            XCTAssertTrue(v.submissions.count > 0,"Submissions were loaded")
+            XCTAssertTrue(v.submissions.count<oldSubmissionsCount)
+            expectation3.fulfill()
+        }
+        
+        v.getSubmissions(submissionsCallBack3)
+        
+        self.waitForExpectationsWithTimeout(5.0) { (error) in
+            if(error != nil) {
+                XCTFail("FAILED due to " + error.description)
+            }
+        }
+        
+    }
+    
     func testLoadCurrentPictureNoPictures()
     {
         // we only have access to this if we import our project above
